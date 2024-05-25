30db30fbd648fd723ad4847269cd3fbf72905928
2001085 <hcalvet70@gmail.com>
2024-05-25 12:27:53 -0400

    On répare les shaders

--- LastStandGameJam/Assets/Scripts/Player/ThirdPersonShooterController.cs
+++ LastStandGameJam/Assets/Scripts/Player/ThirdPersonShooterController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using StarterAssets;
+using UnityEngine.InputSystem;
 
 public class ThirdPersonShooterController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
-    [SerializeField] private ThirdPersonController thirdPersonController;
-    private StarterAssetsInputs starterAssetsInputs;
+    [SerializeField] private float normalSensitivity;
+    [SerializeField] private float aimSensitivity;
+    [SerializeField] private LayerMask aimColliderMask;
+    [SerializeField] private Transform aimTransform;
 
-    public float normalSensitivity;
-    public float aimSensitivity;
+    private ThirdPersonController thirdPersonController;
+    private StarterAssetsInputs starterAssetsInputs;
 
     private void Awake()
     {
@@ -21,6 +24,12 @@ public class ThirdPersonShooterController : MonoBehaviour
     }
     private void Update()
     {
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if(Physics.Raycast(ray, out RaycastHit hitInfo, 999f, aimColliderMask))
+        {
+            aimTransform.position = hitInfo.point;
+        }
         if (starterAssetsInputs.aim)
         {
             aimVirtualCamera.gameObject.SetActive(true);
@@ -31,6 +40,5 @@ public class ThirdPersonShooterController : MonoBehaviour
             aimVirtualCamera.gameObject.SetActive(false);
             thirdPersonController.Sensitivity = normalSensitivity;
         }
-        
     }
 }
